3ea0b3310565dbd898f4dec7d7759c3596563312
Stanley Fuller <stanthesoupking@gmail.com>
2019-07-19 17:02:18 +1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+/**
+ * Generates the given number in the Fibonacci Sequence
+ * 
+ * Author: Stanley Fuller
+ */
+
+use std::io;
+
+fn main() {
+    println!("What fibonacci number would you like to generate?");
+
+    let n: usize;
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed reading line.");
+        n = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Error: '{}' is not a valid integer (>= 0).", input.trim());
+                continue;
+            }
+        };
+        break;
+    }
+
+    let result = fib(n);
+
+    println!("Result is: {}", result);
+}
+
+fn fib(n: usize) -> usize {
+    let mut m: Vec<usize> = Vec::new();
+
+    // Push initial values of fib
+    m.push(1);
+    m.push(1);
+
+    // Do we need to generate further values
+    if n > 1 {
+        for i in 1..n {
+            // Add previous 2 indicies
+            let v = m[i] + m[i - 1];
+
+            m.push(v);
+        }
+    }
+
+    // Return final fib number
+    m[n]
+}
